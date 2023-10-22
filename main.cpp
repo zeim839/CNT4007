5812858bb8d6eb6aa458f71fced4e76570532e7e
zeim839 <zeim839@users.noreply.github.com>
2023-10-22 19:25:14 -0400

    added network example

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <system.hpp>
+#include <network.hpp>
 
 int main()
 {
-	std::cout << "hello world" << std::endl;
+	// Start TCP server.
+	Server srv(3000);
+	std::cout << "server listening on port 3000" << std::endl;
+	while (true) {
+		Connection* conn = srv.listen();
+		std::cout << "incoming connection" << std::endl;
+
+		// 1 KB message buffer.
+		Byte buffer[1024];
+
+		// Receive message.
+		while (conn->receive(buffer, 1024)) {
+			std::string str((char*)buffer);
+			std::cout << "message from peer: " << str << std::endl;
+		}
+	}
 	return 0;
 }
