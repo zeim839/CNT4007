8d31e9b6d9a1ed71c2a422177de473ff1d4d3109
zeim839 <zeim839@users.noreply.github.com>
2023-11-28 22:41:22 -0500

    refactor main.cpp to call Process

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
-#include <system.hpp>
-#include <network.hpp>
-#include <process.hpp>
-#include <chrono>
+#include "src/Protocol.hpp"
+#include "src/PeerProcess.hpp"
 
 int main(int argc, char** argv)
 {
@@ -11,27 +9,11 @@ int main(int argc, char** argv)
 	}
 
 	// Extract ID from cmd arguments.
-	int ID;
-	try { ID = std::stoi(argv[1]); } catch (...) {
+	unsigned int id;
+	try { id = std::stoi(argv[1]); } catch (...) {
 		std::cout << "usage: ./peerProcess [port]" << std::endl;
 		return EXIT_FAILURE;
 	}
 
-	// Use TCP port 6008.
-	int port = 6008;
-
-	// Fetch common.cfg.
-	CFG_COMMON config = loadCommonConf("./common.cfg");
-
-	// Fetch Peerinfo.cfg.
-	std::vector<CFG_PEER> peers = loadPeerConf("./Peerinfo.cfg");
-
-	// Start peer process.
-	PeerProcess p(ID, port, config, peers);
-
-	std::cout << "server listening on port 3001" << std::endl;
-	std::this_thread::sleep_for(std::chrono::seconds(30));
-	p.terminate();
-
-	return EXIT_SUCCESS;
+	PeerProcess process(id);
 }
